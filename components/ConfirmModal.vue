73856ffbc6ac05c885eea022226c60611c5116ae
ratanont-otm <ratanont.s@gmail.com>
2023-02-23 18:37:40 +0700

    update

---
+++ components/ConfirmModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h4>การสั่งซื้อล่วงหน้าสำเร็จ</h4>
+        <p>การชำระเงินของคุณสำเร็จแล้ว!</p>
+        <p>ตอนนี้เราจะส่งอีเมลยืนยันคำสั่งซื้อสำเร็จให้คุณ</p>
+        <button class="bg-lime-500 hover:bg-lime-600 text-white font-bold py-3 px-7 rounded-lg w-full border-0">ยืนยัน</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
